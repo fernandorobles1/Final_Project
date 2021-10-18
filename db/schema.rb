a13a6b06fce38cea4e503c1614325deacea186b2
fernandorobles1 <fdo.robles88@gmail.com>
2021-10-18 19:16:15 -0300

    feat: add Discussion_id to Channel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_215610) do
+ActiveRecord::Schema.define(version: 2021_10_18_221516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_10_18_215610) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "discussion_id"
   end
 
   create_table "discussions", force: :cascade do |t|
@@ -27,12 +28,15 @@ ActiveRecord::Schema.define(version: 2021_10_18_215610) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "channel_id"
   end
 
   create_table "replies", force: :cascade do |t|
     t.text "response"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "discussion_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
